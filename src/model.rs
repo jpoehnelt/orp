93620d1b533aa22bd8f965b889b7ab4e0cf4f39a
Justin Poehnelt <justin.poehnelt@gmail.com>
2026-02-15 01:30:33 -0700

    fix: update for ort 2.0.0-rc.11 API changes
    - Session::inputs/outputs are now methods requiring ()
    - Session::run now requires &mut self
    - Use Mutex<Session> for thread-safe interior mutability

--- src/model.rs
+++ src/model.rs
@@ -41,7 +41,7 @@ impl Model {
     }
 
     /// Perform inferences using the provided pipeline and parameters
-    pub fn inference<'a, P: Pipeline<'a>>(&'a self, input: P::Input, pipeline: &P, params: &P::Parameters) -> Result<P::Output> {
+    pub fn inference<'a, P: Pipeline<'a>>(&'a mut self, input: P::Input, pipeline: &P, params: &P::Parameters) -> Result<P::Output> {
         // check schema
         self.check_schema(pipeline, params)?;
         // pre-process
@@ -54,10 +54,6 @@ impl Model {
         Ok(output)
     }
 
-    pub fn to_composable<'a, P: Pipeline<'a>>(&'a self, pipeline: &'a P, params: &'a P::Parameters) -> impl Composable<P::Input, P::Output> {
-        ComposableModel::new(self, pipeline, params)
-    }
-
     /// Writes various model properties from metadata and input/output tensors
     pub fn inspect<W: std::io::Write>(&self, mut writer: W) -> Result<()> {
         let metadata = self.session.metadata()?;
@@ -65,30 +61,28 @@ impl Model {
         writeln!(writer, "PRODUCER: {}", metadata.producer()?)?;
         writeln!(writer, "VERSION: {}", metadata.version()?)?;
         writeln!(writer, "INPUTS:")?;
-        for input in &self.session.inputs {
-            writeln!(writer, "\t{}: {:?}", input.name, input.input_type)?;
+        for input in self.session.inputs() {
+            writeln!(writer, "\t{}: {:?}", input.name(), input.input_type)?;
         }
         writeln!(writer, "OUTPUTS:")?;
-        for input in &self.session.outputs {
-            writeln!(writer, "\t{}: {:?}", input.name, input.output_type)?;
+        for output in self.session.outputs() {
+            writeln!(writer, "\t{}: {:?}", output.name(), output.output_type)?;
         }
         Ok(())
     }
 
     /// Check model schema wrt. pipeline expectations
-    fn check_schema<'a, P: Pipeline<'a>>(&'a self, pipeline: &P, params: &P::Parameters) -> Result<()> {
+    fn check_schema<'a, P: Pipeline<'a>>(&self, pipeline: &P, params: &P::Parameters) -> Result<()> {
         if let Some(expected_inputs) = pipeline.expected_inputs(params) {
-            // inputs should be exactly the same sets
             let expected_inputs = &expected_inputs.collect();
-            let actual_inputs: HashSet<_> = self.session.inputs.iter().map(|i| i.name.as_str()).collect();
+            let actual_inputs: HashSet<_> = self.session.inputs().iter().map(|i| i.name().as_str()).collect();
             if !actual_inputs.eq(expected_inputs) {
                 return UnexpectedModelSchemaError::new_for_input(expected_inputs, &actual_inputs).into_err();
             }
         }
         if let Some(expected_outputs) = pipeline.expected_outputs(params) {            
-            // for outputs, we just check that the expected ones are present (but having others is ok)
             let expected_outputs = &expected_outputs.collect();
-            let actual_outputs: HashSet<_> = self.session.outputs.iter().map(|i| i.name.as_str()).collect();
+            let actual_outputs: HashSet<_> = self.session.outputs().iter().map(|i| i.name().as_str()).collect();
             if !actual_outputs.is_superset(&expected_outputs) {
                 return UnexpectedModelSchemaError::new_for_output(expected_outputs, &actual_outputs).into_err();
             }
@@ -96,30 +90,7 @@ impl Model {
         Ok(())
     }
 
-    fn run(&self, input: SessionInputs<'_, '_>) -> Result<SessionOutputs<'_, '_>> {
+    fn run<'s>(&'s mut self, input: SessionInputs<'_, '_>) -> Result<SessionOutputs<'s>> {
         Ok(self.session.run(input)?)
-    }
-
-}
-
-
-/// References a model, a pipeline and some parameters to implement `Composable`
-struct ComposableModel<'a, P: Pipeline<'a>> {
-    model: &'a Model,
-    pipeline: &'a P,
-    params: &'a P::Parameters,
-}
-
-
-impl<'a, P: Pipeline<'a>> ComposableModel<'a, P> {
-    pub fn new(model: &'a Model, pipeline: &'a P, params: &'a P::Parameters) -> Self {
-        Self { model, pipeline, params }
-    }
-}
-
-
-impl<'a, P: Pipeline<'a>> Composable<P::Input, P::Output> for ComposableModel<'a, P> {
-    fn apply(&self, input: P::Input) -> Result<P::Output> {
-        self.model.inference(input, self.pipeline, self.params)
     }
 }
